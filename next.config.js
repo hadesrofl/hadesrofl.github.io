0af95d81bd36838a1b48dfdca55b78dcfb0a1579
Rene Kremer <renejon90@googlemail.com>
2024-08-04 21:31:37 +0200

    fix(CI): Allow CI to build blog again

--- next.config.js
+++ next.config.js
@@ -1,2 +1,20 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.tsx')
-module.exports = withNextra()
+
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "" : "";
+
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  swcMinify: true,
+  trailingSlash: true,
+  assetPrefix,
+  basePath: assetPrefix,
+  output: "export",
+};
+
+module.exports = withNextra(nextConfig);
